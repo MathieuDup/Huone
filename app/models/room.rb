9082992a93d048be689a07dfed82203f021fb3b0
Mathieu Dupin <dupin.mathieu@gmail.com>
2022-06-23 17:04:06 +0200

    Add dotenv - Protect my secret data in .env file

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,7 @@ class Room < ApplicationRecord
   belongs_to :hotel
 
   has_many :bookings, dependent: :destroy
+  has_one_attached :photo
 
   validates :capacity, presence: true
   validates :capacity, numericality: { only_integer: true, greater_than: 0 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding database..."
+
+User.create(email: "admin@huone.com", password: "secret")
+
+Hotel.create(name: "Keekoo", address: "Esplanade Charles de Gaulle, Bordeaux")
+Hotel.create(name: "Papa Shelter", address: "Cours du Chapeau Rouge, Bordeaux")
+Hotel.create(name: "Hotel Gerent", address: "Place de la Comédie, Bordeaux")
+Hotel.create(name: "Foology", address: "Cours de la Marne, Bordeaux")
+Hotel.create(name: "Anti Hotel", address: "Quai des Chartrons, Bordeaux")
+
+Room.create(hotel_id: 1, capacity: 1, price_per_night: 40).photo = "room1.jpg"
+Room.create(hotel_id: 2, capacity: 1, price_per_night: 60).photo = "room2.jpg"
+Room.create(hotel_id: 3, capacity: 2, price_per_night: 80).photo = "room3.jpg"
+Room.create(hotel_id: 4, capacity: 4, price_per_night: 150).photo = "room4.jpg"
+Room.create(hotel_id: 5, capacity: 2, price_per_night: 90).photo = "room5.jpg"
